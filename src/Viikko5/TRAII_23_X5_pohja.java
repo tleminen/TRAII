b6cc3077ef534a03c3565c6ecda65cde027b3ca9
Tomi Leminen <tleminen@student.uef.fi>
2023-11-20 13:48:24 +0200

    TraII X5 Tehtava

--- src/Viikko5/TRAII_23_X5_pohja.java
+++ src/Viikko5/TRAII_23_X5_pohja.java
@@ -33,59 +33,61 @@ public class TRAII_23_X5_pohja implements TRAII_23_X5 {
         HashSet<List<Vertex>> tulos = new HashSet<>();
         // TODO: tÃ¤stÃ¤ alkaa polkujen haku, apumetodeja saa ja kannattaa kÃ¤yttÃ¤Ã¤
 
-        float painot;
-        for(Vertex v : lahtoSolmu.neighbors()){
-            //Väritetään solmut valkoisiksi
-            varitaSolmut(verkko, DiGraph.WHITE);
-            //Väritetään lähtösolmu harmaaksi
-            lahtoSolmu.setColor(DiGraph.GRAY);
-            painot = 0.0F;
+        //Väritetään solmut valkoisiksi
+        varitaSolmut(verkko, DiGraph.WHITE);
+        //Käydään lähtösolmun naapurit läpi
+        for(Vertex solmu : lahtoSolmu.neighbors()){
+            if(lahtoSolmu.getEdge(solmu).getWeight()<=maxPaino){
+                //Tehdään lista solmuja varten
+                List<Vertex> listaSolmuille = new LinkedList<>();
+                //Lisätään lähtösolmu listaan
+                listaSolmuille.add(lahtoSolmu);
+                //Väritetään lähtösolmu harmaaksi
+                lahtoSolmu.setColor(DiGraph.GRAY);
 
-            //Tehdään lista solmuja varten
-            List<Vertex> listaSolmuille = new LinkedList<>();
-            //Lisätään lähtösolmu listaan
-            listaSolmuille.add(lahtoSolmu);
-
-            if(lahtoSolmu.getEdge(v).getWeight()<=maxPaino){
-                //Lisätään solmujen välinen paino talteen
-                painot = lahtoSolmu.getEdge(v).getWeight();
-                etsiPolut(lahtoSolmu,v,maxPaino,tulos,listaSolmuille, painot);
-
-            } else if (lahtoSolmu.getEdge(v).getWeight()==maxPaino) {
+                //Lisätään listaan lähtösolmun naapurisolmu
+                listaSolmuille.add(solmu);
                 tulos.add(listaSolmuille);
+
+                etsiPolut(solmu,maxPaino,tulos,listaSolmuille, lahtoSolmu.getEdge(solmu).getWeight());
+                //Värjätään lähtösolmu valkoiseksi
+                lahtoSolmu.setColor(DiGraph.WHITE);
+                //Värjätään lähtösolmun naapurisolmu valkoiseksi
+                solmu.setColor(DiGraph.WHITE);
+
             }
         }
         return tulos;
     }
     private static void varitaSolmut(AbstractGraph g, int vari) {
+        //Värjätään verkon kaikki solmut for each loopissa
         for (Vertex v : g.vertices())
             v.setColor(vari);
     }
-    private static void etsiPolut(Vertex edellinenSolmu, Vertex solmu, float maxPaino, HashSet<List<Vertex>> tulos, List<Vertex> listaSolmuille, float painot){
+    private static void etsiPolut(Vertex solmu, float maxPaino, HashSet<List<Vertex>> tulos, List<Vertex> listaSolmuille, float painot){
+        //Värjätään solmu harmaaksi
         solmu.setColor(DiGraph.GRAY);
-        for(Edge kaari : solmu.edges()){
-            if(painot + kaari.getWeight()>maxPaino && kaari.getEndPoint(solmu) != edellinenSolmu){
-                break;
-            }
-            if(kaari.getEndPoint(solmu).getColor() == DiGraph.GRAY && kaari.getEndPoint(solmu) != edellinenSolmu){
-                break;
-            }
-            //Jos käsiteltävän kaaren päätesolmu on valkoinen ja paino on alle maxpainon
-            if(kaari.getEndPoint(solmu).getColor() == DiGraph.WHITE && painot + kaari.getWeight()<=maxPaino){
-                if(!listaSolmuille.contains(solmu)){
-                    listaSolmuille.add(solmu);
-                }
-                edellinenSolmu = solmu;
-                //Lisätään käsiteltävän kaaren päätesolmu listaan
-                listaSolmuille.add(kaari.getEndPoint(solmu));
-                //Kasvatetaan painoa
-                painot = painot + kaari.getWeight();
 
-                List<Vertex> lista = new LinkedList<>(listaSolmuille);
-                //Lisätään lista hashsettiin
-                tulos.add(listaSolmuille);
-                etsiPolut(edellinenSolmu, kaari.getEndPoint(solmu), maxPaino, tulos, lista, painot);
-                solmu.setColor(DiGraph.WHITE);
+        //Käydään solmun kaaret läpi
+        for(Edge kaari : solmu.edges()){
+
+            //Jos käsiteltävän kaaren päätesolmu on valkoinen niin...
+            if(kaari.getEndPoint(solmu).getColor() == DiGraph.WHITE){
+                //Mikäli tallennettu paino + käsiteltävän kaaren paino on pienempi tai yhtäsuuri kuin maxpaino
+                if(painot + kaari.getWeight()<=maxPaino){
+                    //Luodaan uusi lista uutta polkua varten
+                    List<Vertex> lista = new LinkedList<>(listaSolmuille);
+
+                    //Lisätään käsiteltävän kaaren päätesolmu listaan
+                    lista.add(kaari.getEndPoint(solmu));
+                    //Lisätään lista hashsettiin
+                    tulos.add(lista);
+
+                    //Tehdään rekursio kutsu ja lisätään paino muuttujan käsiteltävän kaaren paino
+                    etsiPolut(kaari.getEndPoint(solmu), maxPaino, tulos, lista, painot+kaari.getWeight());
+                    //Värjätään kaaren päätesolmu valkoiseksi
+                    kaari.getEndPoint(solmu).setColor(DiGraph.WHITE);
+                }
             }
         }
     }
